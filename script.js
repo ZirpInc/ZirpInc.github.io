2d07954a9d2e4cf6fd13719dcf4a20f1e2d91f34
ZirpInc <ZirpInc@Gmail.com>
2015-09-17 05:58:39 -0700

    The base of the website

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Zirp.Inc -  All Rights Reserved</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='about.html'>Data</a></li>" +
+"<li><a href='games.html'>About</a></li>" +
+"</ul>";
